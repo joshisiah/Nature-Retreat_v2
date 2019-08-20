fd0374ae2eafa10f746c0d0b52a47c158d960447
McBride <josh.jay0308@gmail.com>
2019-08-20 16:31:10 -0400

    Added animations

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,11 @@
-console.log('this works');
+const hero = $('.hero');
+const slider = $('.slider');
+const logo = $('#logo');
+const burger = $('.burger');
+const headline = $('.headline');
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero,1.5,{height:'0%'},{height:'80%', ease: Power3.easeInOut})
+.fromTo(hero,1.2,{width:'100%'},{width:'80%', ease: Power3.easeInOut})
+.fromTo(slider,1.2,{x:'-100%'},{x:'0%', ease: Power3.easeInOut}, '-=1')
